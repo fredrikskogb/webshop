3f270a74f59c6bce97d642ca4c72d31b2130d03c
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-04-25 13:27:56 +0200

    will now loop products on home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,8 @@ import { MoviesComponent } from './movies/movies.component';
     MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IMovie } from '../interfaces/IMovie';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
+
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  movies: IMovie[];
+
+  constructor(private service: DataService) { }
 
   ngOnInit() {
+    this.service.getData().subscribe((data) => { this.movies = data; });
   }
 
 }
