9e7a6ad3ba1f1d1fa01a7a728c7bdc56bd6cff8a
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-05-14 01:33:35 +0200

    now calculates total price

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -2,6 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormComponent } from './form.component';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
+import { IMovie } from '../interfaces/IMovie';
+import { MockDataService } from '../services/mock-data.service';
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -19,9 +22,22 @@ describe('FormComponent', () => {
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    const mockMovie: IMovie = new MockDataService().movies[0];
+    localStorage.setItem('cart', JSON.stringify([mockMovie]));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create order rows array', () => {
+    expect(component.orderRows).toEqual([]);
+  });
+    
+  it('should structure order rows', () => { 
+    component.mapItems();
+    expect(component.orderRows).toEqual([{ ProductId: 76 , Amount: 1 }]);
+    localStorage.removeItem('cart');
+  });
+
 });
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -15,7 +15,7 @@ export class FormComponent implements OnInit {
   cart: IMovie[];
   order: IOrder;
   orderRows: IOrderRows[] = [];
-  totalPrice: number;
+  totalPrice: number = 0;
   customer = this.fb.group({
     companyId: ['', Validators.required],
     createdBy: ['', Validators.required]
@@ -28,22 +28,22 @@ export class FormComponent implements OnInit {
   }
 
   mapItems() {
-
     for(let i = 0; i < this.cart.length; i++){
       let amount: number = 1;
       let newItem: IOrderRows = { ProductId: this.cart[i].id , Amount: amount }
-      this.orderRows.push(newItem);
 
+      this.orderRows.push(newItem);
+      this.totalPrice += this.cart[i].price;
       for(let j = i + 1; j < this.cart.length; j++){
         if(this.orderRows[i].ProductId === this.cart[j].id){
+          this.totalPrice += this.cart[j].price;
           this.cart.splice(j, 1);
           j--
           this.orderRows[i].Amount += 1;
         }
       }
     }
-
-    console.log(this.orderRows);
+    console.log(this.totalPrice);
 
   }
 
@@ -63,7 +63,11 @@ export class FormComponent implements OnInit {
   submitOrder(){
     this.mapItems();
     this.createOrder();
-    console.log(this.totalPrice);
+    localStorage.setItem('cart', JSON.stringify([]));
+    this.cart = [];
+    console.log(this.orderRows);
+    this.orderRows = [];
+    console.log(this.orderRows);
   }
 
 }
