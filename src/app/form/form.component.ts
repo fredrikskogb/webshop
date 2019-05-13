c0a9525c7d8d5d36a611b98ea3254187288f1a00
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-05-13 20:16:38 +0200

    now puts order in new object to push to db

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { IMovie } from '../interfaces/IMovie';
+import { IOrderRows } from '../interfaces/IOrderRows';
+import { CartService } from '../services/cart-service';
 
 
 @Component({
@@ -9,9 +12,14 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
+  cart: IMovie[];
+
+  orderRows: IOrderRows[] = [];
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.cart = JSON.parse(localStorage.getItem('cart'));
   }
 
   order = this.fb.group({
@@ -20,7 +28,29 @@ export class FormComponent implements OnInit {
   });
 
   submitOrder(){
-    
+    this.mapItems();
+
+  }
+
+  mapItems() {
+
+    for(let i = 0; i < this.cart.length; i++){
+
+      let amount: number = 1;
+      let newItem: IOrderRows = { ProductId: this.cart[i].id , Amount: amount }
+      this.orderRows.push(newItem);
+
+      for(let j = i + 1; j < this.cart.length; j++){
+        if(this.orderRows[i].ProductId === this.cart[j].id){
+          this.cart.splice(j, 1);
+          j--
+          this.orderRows[i].Amount += 1;
+        }
+      }
+
+    }
+    console.log(this.orderRows);
+
   }
 
 }
