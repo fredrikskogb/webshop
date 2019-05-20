cc0603b8ac15cb2eadd73893d14de0f530f591a6
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-05-21 00:27:41 +0200

    now updates and deletes order

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,7 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DataService } from '../services/data.service';
+import { MockDataService } from '../services/mock-data.service';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -12,6 +14,7 @@ describe('AdminComponent', () => {
       declarations: [ AdminComponent ],
       imports: [HttpClientModule]
     })
+    .overrideComponent(AdminComponent, { set: { providers: [ {provide: DataService, useClass: MockDataService}]}})
     .compileComponents();
   }));
 
@@ -24,4 +27,20 @@ describe('AdminComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain order', () => {
+    expect(component.order.length).toBe(2);
+  });
+
+  it('should get what movies are in stock', () => {
+    expect(component.movies.length).toBe(2);
+  });
+
+
+  it('should set orderPresentation', () => {
+    component.mapItems();
+    expect(component.orderPresentation.length).toBe(2);
+  });
+
+ 
 });
--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -27,7 +27,8 @@ export class AdminComponent implements OnInit {
         for(let k = 0; k < this.movies.length; k++){
           if(this.order[i].orderRows[j].productId === this.movies[k].id){
             this.movies[k]["amount"] = this.order[i].orderRows[j].amount;
-            this.orderPresentation.push(this.movies[k]);
+            this.movies[k]["orderId"] = this.order[i]["id"];
+            this.orderPresentation.unshift(this.movies[k]);
           }
         }
       }
@@ -36,7 +37,12 @@ export class AdminComponent implements OnInit {
   }
 
   deleteOrder(id: number){
-
+    this.service.deleteOrder(id).subscribe();
+    for (let i = this.orderPresentation.length - 1; i >= 0; --i) {
+      if (this.orderPresentation[i].orderId == id) {
+        this.orderPresentation.splice(i,1);
+      }
+    }
   }
 
 }
