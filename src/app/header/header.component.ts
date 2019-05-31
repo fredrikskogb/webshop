a6138f48a36e37e8616aa4d7e8b19174f6587968
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-05-31 23:03:35 +0200

    fixed bugged for amount of products in cart display

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,20 +14,22 @@ import { DataService } from '../services/data.service';
 export class HeaderComponent implements OnInit {
 
   cart: IMovie[] = [];
-  cartSubscription: Subscription;
+  subscription: Subscription;
   searchValue: string;
 
-  constructor(private cartService: CartService, private router: Router) { }
-
-  // Show amount of products in chart
-  ngOnInit() {
-    this.cartSubscription = this.cartService.getCart().subscribe(value => {
+  constructor(private cartService: CartService, private router: Router) {
+    this.subscription = this.cartService.getCart().subscribe(value => {
       if(this.router.url === '/checkout'){
         this.cart = JSON.parse(localStorage.getItem('cart'));
         return;
       }
       this.cart.push(value);
     });
+  }
+
+  // Show amount of products in chart
+  ngOnInit() {
+    this.cart = JSON.parse(localStorage.getItem("cart"));
   }
 
   // Check if cart is set to dislplay HTML
